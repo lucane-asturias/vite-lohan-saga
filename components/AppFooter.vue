c9ccb01f91fea77bf9816c53f3744fae087dc9c6
Lucane <lucassanntos01@gmail.com>
2024-06-12 02:34:49 -0700

    init

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="p-5 border border-t-slate-400 bg-slate-300/[.06]">
+    <div class="container mx-auto text-center text-black font-[350]">
+      &copy; 2024 Lucane Asturias. All rights reserved.
+    </div>
+  </footer>
+</template>
+
+<script setup>
+// No need for any script logic for now.
+</script>
---
+++ components/BookCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+// No need for any script logic for now.
+</script>
+
+<template>
+  <div class="max-w-sm bg-white rounded-lg mr-4">
+    <img src="https://placehold.co/600x400" alt="Book Cover" class="w-full h-24 object-cover shadow-lg">
+    <div class="p-6">
+      <p class="text-gray-700 mb-4">
+        This is a brief description of my book. It provides an overview of the content and what readers can expect.
+      </p>
+      <button 
+        @click="buyNow" 
+        class="w-full px-4 py-2 
+        bg-[#ffd54f] hover:bg-yellow-300/100
+        text-slate-700 text-xl font-bold 
+        border border-yellow-500 rounded-md mb-4">
+        Buy Now ($89)
+      </button>
+      <button 
+        @click="readSample" 
+        class="
+          w-full px-4 py-2
+          bg-blue-100 hover:bg-blue-200
+          text-slate-700 text-xl font-bold 
+          border border-slate-400 rounded-md
+        ">
+        Read Synopsis
+      </button>
+    </div>
+  </div>
+</template>
